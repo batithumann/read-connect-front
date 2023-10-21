258b6c2a0596f1fb879045c2ba7d4fb5e71138c4
Walter Thumann <wthumann@gmail.com>
2023-10-21 00:52:35 -0300

    add Home view with Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ChakraProvider } from "@chakra-ui/react";
+
+import Context from "./Context";
+import Home from "./views/Home";
 
 function App() {
+  const [usuario, setUsuario] = useState(null);
+  useEffect(() => {
+    const data = localStorage.getItem("email");
+    if (data) setUsuario({ email: data });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Context.Provider value={{ usuario, setUsuario }}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="*" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </Context.Provider>
+    </ChakraProvider>
   );
 }
 
