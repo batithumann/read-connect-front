19b9e948d7ccd2c3da5d98a46f25ac36260feb0d
Walter Thumann <wthumann@gmail.com>
2023-10-25 09:01:07 -0300

    adjust login view style

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,6 +5,7 @@ import {
   Input,
   Button,
   Flex,
+  Container,
   Stack,
   FormControl,
   FormLabel,
@@ -53,60 +54,62 @@ function Login() {
       justify={"center"}
       bg={useColorModeValue("gray.50", "gray.800")}
     >
-      <Stack spacing={8} mx={"auto"} minW={"md"} maxW={"lg"} py={12} px={6}>
-        <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Entra a tu cuenta</Heading>
-        </Stack>
-        <Box
-          rounded={"lg"}
-          bg={useColorModeValue("white", "gray.700")}
-          boxShadow={"lg"}
-          p={8}
-        >
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email</FormLabel>
-              <Input
-                name="email"
-                type="email"
-                value={formState.email}
-                onChange={handleInputChange}
-                required
-              />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input
-                name="password"
-                type="password"
-                value={formState.password}
-                onChange={handleInputChange}
-                required
-              />
-            </FormControl>
-            <Stack spacing={10}>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-                onClick={login}
-              >
-                Entrar
-              </Button>
-              <Flex justifyContent="center">
-                <Text mr="2">
-                  ¿No tienes cuenta?{" "}
-                  <Link href="/register" color={"blue.400"}>
-                    Regístrate
-                  </Link>
-                </Text>
-              </Flex>
-            </Stack>
+      <Container>
+        <Stack spacing={8} mx={"0"} maxW={"lg"} py={12} px={6}>
+          <Stack align={"center"}>
+            <Heading fontSize={"4xl"}>Entra a tu cuenta</Heading>
           </Stack>
-        </Box>
-      </Stack>
+          <Box
+            rounded={"lg"}
+            bg={useColorModeValue("white", "gray.700")}
+            boxShadow={"lg"}
+            p={8}
+          >
+            <Stack spacing={4}>
+              <FormControl id="email">
+                <FormLabel>Email</FormLabel>
+                <Input
+                  name="email"
+                  type="email"
+                  value={formState.email}
+                  onChange={handleInputChange}
+                  required
+                />
+              </FormControl>
+              <FormControl id="password">
+                <FormLabel>Password</FormLabel>
+                <Input
+                  name="password"
+                  type="password"
+                  value={formState.password}
+                  onChange={handleInputChange}
+                  required
+                />
+              </FormControl>
+              <Stack spacing={10}>
+                <Button
+                  bg={"blue.400"}
+                  color={"white"}
+                  _hover={{
+                    bg: "blue.500",
+                  }}
+                  onClick={login}
+                >
+                  Entrar
+                </Button>
+                <Flex justifyContent="center">
+                  <Text mr="2">
+                    ¿No tienes cuenta?{" "}
+                    <Link href="/register" color={"blue.400"}>
+                      Regístrate
+                    </Link>
+                  </Text>
+                </Flex>
+              </Stack>
+            </Stack>
+          </Box>
+        </Stack>
+      </Container>
     </Flex>
   );
 }
